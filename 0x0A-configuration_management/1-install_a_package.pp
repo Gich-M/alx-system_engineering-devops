1f520aa68298b1d8aa8a6e307b3610f0de576dc8
Gich-M <gichmuriuki21@gmail.com>
2024-05-16 10:24:47 -0700

    pup fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 #!/usr/bin/pup
-# Installs flask version (2.1.0) using pip
-package {'flask':
+# Installs flask version 2.1.0 using pip
+package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
+
