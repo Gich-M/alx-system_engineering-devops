40c6ccafddc3cc4eb950bd8b19e00696a5f7f1c0
Gich-M <gichmuriuki21@gmail.com>
2024-08-15 15:43:02 -0700

    lint

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import requests
-
+"""Queries and returns first 10 hot posts of a given subreddit"""
 
 def top_ten(subreddit):
     """Prints the titles of the 10 hottest posts for a given subreddit."""
