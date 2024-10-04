fe175b149b169bf3372a058b52cfc3e4b21f4dca
Gich-M <gichmuriuki21@gmail.com>
2024-10-04 14:38:07 +0300

    API advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Prints the titles of the 10 hottest posts for a given subreddit."""
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot/about.json".format(subreddit)
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/Gongzillah)"
     }
